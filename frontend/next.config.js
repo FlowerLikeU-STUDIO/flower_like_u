215b90820bfd37d9592b223042192952998bcff5
dabeen <elqla19@gmail.com>
2022-10-27 16:22:46 +0900

    Feat: 마이페이지 > 데이터 연결

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,7 +5,7 @@ module.exports = {
   reactStrictMode: true,
   swcMinify: true,
   sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
+    includePaths: [path.join(__dirname, "styles")], // scss 설정
     prependData: `@import "/src/styles/_variables.module.scss"; @import "/src/styles/_mixins.module.scss";`, // prependData 옵션 추가
   },
 };
--- frontend/src/pages/auth/mypage.js
+++ frontend/src/pages/auth/mypage.js
@@ -1,22 +1,68 @@
-import classNames from "classnames";
-import { Fragment } from "react";
-import { useSelector } from "react-redux";
-import Button from "../../styles/Button";
+import classnames from "classnames";
+import Button from "@/styles/Button";
 import styles from "./mypage.module.scss";
+import useSWR from "swr";
+import { useEffect, useState } from "react";
+import ProfileImage from "@/components/mypage/common/ProfileImage";
 
 const Mypage = () => {
-  const role = "seller"; // buyer, seller (data로 받게 될 시 삭제)
-  const isTrue = true;
+  const [userData, setUserData] = useState("");
+  const [userType, setUserType] = useState("buyer");
+
+  // ! useSWR이 데이터 변경되면 자동으로 fetching해줌
+  const { data, error } = useSWR("https://nextjs-course-f7f75-default-rtdb.firebaseio.com/mypage.json", (url) =>
+    fetch(url).then((res) => res.json())
+  );
+
+  useEffect(() => {
+    if (data) {
+      const transFormedData = [];
+      for (const key in data) {
+        /* 
+        ! buyer, seller check
+        if (data[key].response.type === "buyer") {
+          setUserType("buyer");
+        } else if (data[key].response.type === "seller") {
+          setUserType("seller");
+        }
+        (data[0] = seller), [1], buyer;
+        */
+      }
+
+      //! buyer
+      setUserType("buyer");
+      transFormedData.push(data[0].response);
+      setUserData({ ...transFormedData }[0]);
+      // !seller;
+      // setUserType("seller");
+      // transFormedData.push(data[1].response);
+      // setUserData({ ...transFormedData }[0]);
+    }
+  }, [data]);
+
+  if (error) {
+    return <p>No data Yet</p>;
+  }
+
+  if (!data || !userData) {
+    return <p>Loading...</p>;
+  }
+  console.log(userData);
+
   return (
     <div className={styles.layout}>
       <div className={styles.profile}>
-        <div className={styles.profile_img} />
+        {/* <ProfileImage url={userData.profile} size="medium" /> */}
         <div className={styles.flexdiv}>
           <div>
-            <span>닉네임 | 본명</span>
-            <span class="material-icons-outlined">settings</span>
+            {userType === "seller" ? (
+              <span>{userData.store || userData.name}</span>
+            ) : (
+              <span>{userData.nickname || userData.name}</span>
+            )}
+            <span className="material-icons-outlined">settings</span>
           </div>
-          {role === "seller" && (
+          {userType === "seller" && (
             <div className={styles.profile_seller}>
               <p>판매자 프로필 하단</p>
               <p>판매자 프로필 하단</p>
@@ -24,17 +70,17 @@ const Mypage = () => {
           )}
         </div>
       </div>
-      <hr classNames />
+      <hr />
       <div>
-        <Button color="pink">가나다라마바사</Button>
-        <button
-          className={classNames({
+        {/* <Button color="pink">가나다라마바사</Button> */}
+        {/* <button
+          className={classnames({
             [styles.btn_yellow]: isTrue,
           })}
         >
           asdf
-        </button>
-        <button className={`commentBtn ${isTrue ? "btn_yellow" : "btn_default"}`}>asdf</button>
+        </button> */}
+        {/* <button className={`commentBtn ${isTrue ? "btn_yellow" : "btn_default"}`}>asdf</button> */}
         <button>asdf</button>
       </div>
       {/* {role === "seller" ? <사용자 /> : <사업자 />} */}
