79cef0e8b5fdccbda214f0fd239b5573c07c77af
Youji-Sung <anc9432@gmail.com>
2022-10-31 16:59:36 +0900

    Feat: 커스텀 > 동적 라우팅 -> store 이용으로 로직 변경

--- frontend/src/components/custom/step/SelectSize.js
+++ frontend/src/components/custom/step/SelectSize.js
@@ -1,19 +1,33 @@
 import styles from "./SelectSize.module.scss";
 import OptionCard from "../common/OptionCard";
 import { SizeContent } from "./StepContents";
+import { useDispatch } from "react-redux";
+import { selectSize } from "@/store/reducers/custom";
+import { useSelector } from "react-redux";
 
-const SelectSize = (props) => {
-  const packageType = props.package;
-  const contents = SizeContent[packageType];
+const SelectSize = () => {
+  const dispatch = useDispatch();
+
+  //* 패키지 타입에 따라서 사이즈 옵션이 다르다.
+  const stepState = useSelector((state) => state.custom);
+  const packageType = stepState.package;
+  const contents = SizeContent[packageType - 1];
+
+  //* 사이즈 값을 변경하는 함수
+  const sizeHandler = (index) => {
+    dispatch(selectSize(index));
+  };
+
   return (
     <div className={styles.size_wrapper}>
       {contents.title.map((title, index) => (
         <OptionCard
+          key={title}
           size="small"
           img="/home/centerFlower.png"
           title={title}
           content={contents.contents[index]}
-          route={`bouquet/${packageType}/${index}`}
+          handler={() => sizeHandler(index + 1)}
         />
       ))}
     </div>
