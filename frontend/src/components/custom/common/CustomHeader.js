e12a51a7239db35cfab33de30dc0fc0563bc4813
Youji-Sung <anc9432@gmail.com>
2022-11-01 16:52:04 +0900

    Modify: 커스텀 > 커스텀 단계에서 보이는 헤더의 디자인 및 로직 수정

--- frontend/src/components/custom/common/CustomHeader.js
+++ frontend/src/components/custom/common/CustomHeader.js
@@ -1,7 +1,42 @@
 import styles from "./CustomHeader.module.scss";
+import classNames from "classnames/bind";
 
-const CustomHeader = () => {
-  return <header>커스텀 헤더</header>;
+const CustomHeader = (props) => {
+  const cx = classNames.bind(styles);
+  const stepState = props.stepState;
+
+  return (
+    <nav className={styles.custom_header_wrapper}>
+      <div className={styles.step_wrapper}>
+        <div
+          className={
+            !stepState.package && !stepState.size ? cx("number_title_wrapper", "active") : styles.number_title_wrapper
+          }
+        >
+          <div>1</div>
+          <p>포장방법 선택하기</p>
+        </div>
+        <div className={styles.step_line} />
+        <div
+          className={
+            stepState.package && !stepState.size ? cx("number_title_wrapper", "active") : styles.number_title_wrapper
+          }
+        >
+          <div>2</div>
+          <p>사이즈 선택하기</p>
+        </div>
+        <div className={styles.step_line} />
+        <div
+          className={
+            stepState.package && stepState.size ? cx("number_title_wrapper", "active") : styles.number_title_wrapper
+          }
+        >
+          <div>3</div>
+          <p>꽃다발 커스텀</p>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default CustomHeader;
