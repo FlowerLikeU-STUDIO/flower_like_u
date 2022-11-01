852bb0786a156288c60edb3c4d0cb6e2980085f5
강민성 <pfcskms1997@naver.com>
2022-11-01 13:12:33 +0900

    Modify: 파일명 변경 및 미사용 import 코드 제거

--- backend/fly/src/main/java/com/ssafy/fly/service/UserService.java
+++ backend/fly/src/main/java/com/ssafy/fly/service/UserService.java
@@ -2,16 +2,16 @@ package com.ssafy.fly.service;
 
 import com.ssafy.fly.dto.request.*;
 
-public interface AccountService {
+public interface UserService {
     public boolean checkIdDuplication(String inputId);
     public boolean saveMember(RegisterReq registerReq);
     public String findID(FindIdReq findIdReq);
     public boolean issueTemporaryPassword(FindPwdReq findPwdReq);
     public boolean checkNicknameDuplication(String inputNickname);
-    public boolean updateAccountInfo(ChangeInfoReq changeInfoReq);
+    public boolean updateUserInfo(ChangeInfoReq changeInfoReq);
     public boolean updateIntroduction(ChangeIntroductionReq changeIntroductionReq);
     public boolean updatePassword(ChangePwdReq changePwdReq);
     public boolean updateProfileImage(ChangeProfileReq changeProfileReq);
-    public boolean deleteAccount(WithdrawReq withdrawReq);
-    public Object findAccountInfo(String userId);
+    public boolean deleteUser(WithdrawReq withdrawReq);
+    public Object findUserInfo(String userId);
 }
