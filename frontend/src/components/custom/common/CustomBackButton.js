8defbaf65f2255afedf20b958b78e743813e1f82
Youji-Sung <anc9432@gmail.com>
2022-11-01 16:51:20 +0900

    Feat: 커스텀 > 커스텀 단계에서의 전 단계로 돌아갈 수 있는 백버튼 추가

--- frontend/src/components/custom/common/CustomBackButton.js
+++ frontend/src/components/custom/common/CustomBackButton.js
@@ -1,5 +1,43 @@
+import { useDispatch, useSelector } from "react-redux";
+import { selectPackage } from "@/store/reducers/custom";
+import { useRouter } from "next/router";
+import styles from "./CustomBackButton.module.scss";
+
 const CustomBackButton = () => {
-  return <button className="material-icons-outlined">arrow_back</button>;
+  const router = useRouter();
+  const dispatch = useDispatch();
+
+  //* 패키지 값을 다시 null로 초기화 후 router.push
+  const packageHandler = () => {
+    dispatch(selectPackage(null));
+    router.push("/custom");
+  };
+
+  //* 패키지 값을 다시 null로 초기화
+  const sizeHandler = () => {
+    dispatch(selectPackage(null));
+  };
+
+  //* 현재 state 값 확인
+  const stepState = useSelector((state) => state.custom);
+
+  return (
+    <>
+      {stepState.package === null ? (
+        <div className={styles.custom_backbutton} onClick={() => packageHandler()}>
+          <button className="material-icons-outlined">arrow_back</button>
+        </div>
+      ) : stepState.size === null ? (
+        <div className={styles.custom_backbutton} onClick={() => sizeHandler()}>
+          <button className="material-icons-outlined">arrow_back</button>
+        </div>
+      ) : (
+        <div className={styles.invisible}>
+          <button className="material-icons-outlined">arrow_back</button>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default CustomBackButton;
