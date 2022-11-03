57996da71663aac36ede88b285bf4d59255e6b90
dabeen <elqla19@gmail.com>
2022-11-03 10:07:23 +0900

    Feat: hooks > API연결, 회원정보수정로직 추가

--- frontend/src/hooks/useUser.js
+++ frontend/src/hooks/useUser.js
@@ -1,18 +1,29 @@
-import axios from "axios";
+import { Fetcher } from "@/api/axios";
 import useSWR from "swr";
 
-const userFetcher = () => {
-  return axios.get("/users").then((res) => res);
-};
+// ! FETCH USER DATA
 const useUser = () => {
-  const { data, mutate, error } = useSWR("user", userFetcher);
+  const uid = "useruser1";
+  const { data, mutate, error } = useSWR(`user/${uid}`, Fetcher);
 
   const loading = !data && !error;
   const loggedOut = error && error.status === 403;
   return {
     loading,
     loggedOut,
-    user: data ? data.data : data,
+    // user: data ? data.data : data, // -> runtimeerror
+    user: loading
+      ? ""
+      : {
+          type: "consumer",
+          userId: "useruser1",
+          name: "김싸피",
+          nickname: "닉네임",
+          email: "useruser1@gmail.com",
+          address: "대전광역시 유성구 XXX",
+          profile: "",
+          regDate: "2022-10-25",
+        }, //data.data.userInfo,
     mutate,
   };
 };
