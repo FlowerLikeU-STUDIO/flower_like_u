54da61b36d6d60ac6ed18534cf5b62040c42e03f
dabeen <elqla19@gmail.com>
2022-11-03 16:26:27 +0900

    Feat: 회원정보수정 url 분리

--- frontend/src/pages/mypage/settings/index.js
+++ frontend/src/pages/mypage/settings/index.js
@@ -1,19 +1,30 @@
 import MySetting from "@/components/auth/MySetting";
 import Button from "@/components/common/Button";
+import useUser from "@/hooks/useUser";
 
 const MyPageSettings = () => {
+  const { user } = useUser();
+
   return (
-    <MySetting>
-      <Button link="/mypage/settings/modify" size="setting-index" color="mypage-setting">
-        내 정보수정
-      </Button>
-      <Button link="/mypage/settings/password" size="setting-index" color="mypage-setting">
-        비밀번호 수정
-      </Button>
-      <Button link="/mypage/settings/withdrawal" size="setting-index" color="mypage-setting">
-        회원탈퇴
-      </Button>
-    </MySetting>
+    <>
+      {user && (
+        <MySetting>
+          <Button
+            link={user.type === "consumer" ? "/mypage/settings/modify" : "/mypage/settings/modify-store"}
+            size="setting-index"
+            color="mypage-setting"
+          >
+            내 정보수정
+          </Button>
+          <Button link="/mypage/settings/password" size="setting-index" color="mypage-setting">
+            비밀번호 수정
+          </Button>
+          <Button link="/mypage/settings/withdrawal" size="setting-index" color="mypage-setting">
+            회원탈퇴
+          </Button>
+        </MySetting>
+      )}
+    </>
   );
 };
 
