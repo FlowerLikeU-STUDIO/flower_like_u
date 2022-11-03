893dc8f9ea20c2f891423cbec14a93fc0f49a9c1
dabeen <elqla19@gmail.com>
2022-11-03 10:13:53 +0900

    Feat: 회원정보수정 > 공용 레이아웃 컴포넌트 추가

--- frontend/src/components/auth/MySetting.js
+++ frontend/src/components/auth/MySetting.js
@@ -1,29 +1,30 @@
 import MyWrapper from "@/components/common/MyWrapper";
 import ProfileImage from "@/components/common/ProfileImage";
-import useMypage from "@/hooks/useMypage";
+import useUser from "@/hooks/useUser";
 import styles from "./MySetting.module.scss";
 import classnames from "classnames/bind";
 
 const MyPageSettings = ({ children }) => {
   const cx = classnames.bind(styles);
 
-  // *profile 정보 hook에서 불러오기
-  const uid = "mypage-buyer";
-  // const uid = "mypage-seller";
-  const { data, type } = useMypage(uid);
-  // console.log(data, type);
-  // console.log(data);
+  const { user } = useUser("useruser1");
+  // console.log(user);
+
   return (
-    <MyWrapper>
-      <div className={styles.main__div}>
-        <div className={styles.header__flex}>
-          <div className={styles.yellow__circle} />
-          <ProfileImage url={data.profile} size="medium" isModify="modifyProfile" />
-          <span className={cx("material-icons-outlined", "settings")}>settings</span>
-        </div>
-        <div className={styles.info__div}>{children}</div>
-      </div>
-    </MyWrapper>
+    <>
+      {user && (
+        <MyWrapper>
+          <div className={styles.main__div}>
+            <div className={styles.header__flex}>
+              <div className={styles.yellow__circle} />
+              <ProfileImage url={user.profile} size="medium" isModify="modifyProfile" />
+              <span className={cx("material-icons-outlined", "settings")}>settings</span>
+            </div>
+            <div className={styles.info__div}>{children}</div>
+          </div>
+        </MyWrapper>
+      )}
+    </>
   );
 };
 
