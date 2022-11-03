a333828878eb2bc2fa286e0b197e8e51239316dd
dabeen <elqla19@gmail.com>
2022-11-03 10:05:47 +0900

    Feat: 주소정보 수정로직 추가

--- frontend/src/components/common/GetRoad.js
+++ frontend/src/components/common/GetRoad.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./GetRoad.module.scss";
 //https://postcode.map.daum.net/guide
 
 const GetRoadAdr = ({ adr, setAdr }) => {
+  const [roadDetail, setRoadDetail] = useState("");
+
   function sample4_execDaumPostcode() {
     new daum.Postcode({
       oncomplete: function (data) {
@@ -24,39 +26,36 @@ const GetRoadAdr = ({ adr, setAdr }) => {
         // *상세주소 추가로직
         // document.getElementById("sample4_detailAddress").value = extraRoadAddr;
         // !저장 로직
-        setAdr({ zipCode: data.zonecode, street: roadAddr, detail: "", sigunguCode: data.sigunguCode });
-
-        var guideTextBox = document.getElementById("guide");
-        if (data.autoRoadAddress) {
-          var expRoadAddr = data.autoRoadAddress + extraRoadAddr;
-          guideTextBox.innerHTML = "(예상 도로명 주소 : " + expRoadAddr + ")";
-          guideTextBox.style.display = "block";
-        } else if (data.autoJibunAddress) {
-          var expJibunAddr = data.autoJibunAddress;
-          guideTextBox.innerHTML = "(예상 지번 주소 : " + expJibunAddr + ")";
-          guideTextBox.style.display = "block";
-        } else {
-          guideTextBox.innerHTML = "";
-          guideTextBox.style.display = "none";
-        }
+        setAdr({ zipCode: data.zonecode, street: roadAddr, detail: roadDetail, sigunguCode: data.sigunguCode });
       },
     }).open();
   }
 
   const getDetail = (e) => {
-    // !상세주소
-    console.log(e.target.value);
+    const value = e.target.value;
+    if (value.length > 50) {
+      setRoadDetail(roadDetail.slice(0, 50));
+      alert("50자 내로 입력해주세요.");
+      return;
+    }
+    setRoadDetail(value);
+    setAdr({ ...adr, detail: value });
   };
 
   return (
     <div className={styles.road__flex}>
       <div className={styles.post__flex}>
-        <input type="text" id="sample4_postcode" className={styles.post} placeholder="우편번호" disabled />
+        <input type="text" id="sample4_postcode" placeholder="우편번호" disabled />
         <input type="button" onClick={sample4_execDaumPostcode} className={styles.post__btn} value="우편번호 찾기" />
       </div>
-      <input type="text" id="sample4_roadAddress" placeholder="도로명주소" disabled />
-      <span id="guide" style={{ color: "#999", display: "none" }}></span>
-      <input type="text" id="sample4_detailAddress" placeholder="상세주소를 입력해주세요." onChange={getDetail} />
+      <textarea id="sample4_roadAddress" className={styles.address__detail} placeholder="도로명주소" disabled />
+      <textarea
+        id="sample4_detailAddress"
+        className={styles.address__detail}
+        placeholder="상세주소를 입력해주세요."
+        onChange={getDetail}
+        value={roadDetail}
+      />
     </div>
   );
 };
