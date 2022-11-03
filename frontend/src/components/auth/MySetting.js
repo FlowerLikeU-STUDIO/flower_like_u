465b6d215d206af54bc9eb44e7d5c1fbb8d3f048
dabeen <elqla19@gmail.com>
2022-11-03 16:09:57 +0900

    Feat: 회원정보수정 > 사진업로드 로직

--- frontend/src/components/auth/MySetting.js
+++ frontend/src/components/auth/MySetting.js
@@ -3,12 +3,55 @@ import ProfileImage from "@/components/common/ProfileImage";
 import useUser from "@/hooks/useUser";
 import styles from "./MySetting.module.scss";
 import classnames from "classnames/bind";
+import { useEffect, useRef, useState } from "react";
+import Axios from "@/api/axios";
 
 const MyPageSettings = ({ children }) => {
   const cx = classnames.bind(styles);
+  const { user, mutate } = useUser();
+  const [userImg, setUserImg] = useState("");
+  const imgInputRef = useRef();
 
-  const { user } = useUser();
-  console.log(user);
+  useEffect(() => {
+    if (!user) {
+      return;
+    }
+    setUserImg(userImg ? userImg : user.profile);
+  }, [user]);
+
+  const putImg = async () => {
+    const data = {
+      userId: user.userId,
+      image: userImg,
+    };
+    const res = await Axios.put("user/changeImg", data).then((res) => res.data);
+    if (res.result === "success") {
+      alert("이미지가 성공적으로 변경되었습니다.");
+      // TODO mutate로직 생각해보기
+      mutate();
+    } else {
+      alert("실패");
+    }
+  };
+
+  const uploadImg = () => {
+    const element = imgInputRef.current.files[0];
+    if (imgInputRef.current.files.length === 0) {
+      return;
+    }
+    var reader = new FileReader();
+    reader.readAsDataURL(element);
+    reader.onloadend = function () {
+      const res = reader.result;
+      console.log("RESULT", res);
+      setUserImg(res);
+      putImg();
+    };
+    reader.onerror = function () {
+      console.log("there are some problems");
+    };
+  };
+
   return (
     <>
       {user && (
@@ -16,8 +59,13 @@ const MyPageSettings = ({ children }) => {
           <div className={styles.main__div}>
             <div className={styles.header__flex}>
               <div className={styles.yellow__circle} />
-              <ProfileImage url={user.profile || ""} size="medium" isModify="modifyProfile" />
-              <span className={cx("material-icons-outlined", "settings")}>settings</span>
+              <ProfileImage url={userImg} size="medium" isModify="modifyProfile" />
+              <div className={styles.image__upload}>
+                <label htmlFor="file-input">
+                  <span className={cx("material-icons-outlined", "settings")}>settings</span>
+                </label>
+                <input id="file-input" type="file" ref={imgInputRef} onChange={uploadImg} accept="image/*" />
+              </div>
             </div>
             <div className={styles.info__div}>{children}</div>
           </div>
