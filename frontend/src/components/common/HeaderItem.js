8a5a8a71f79aae334f230b1f5f5585c6c3f8c686
dabeen <elqla19@gmail.com>
2022-11-04 13:05:07 +0900

    Feat: 공용 헤더아이템 분리

---
+++ frontend/src/components/common/HeaderItem.js
@@ -0,0 +1,37 @@
+import styles from "./HeaderItem.module.scss";
+import ProfileImage from "@/components/common/ProfileImage";
+import Contents from "./Contents";
+import Link from "next/link";
+import React from "react";
+
+const HeaderItem = ({ ...props }) => {
+  console.log(props.isMyPage); // mypage에선 props.isMyPage=true
+
+  return (
+    <div className={styles.header__main}>
+      <ProfileImage url={props.profile} size="small_medium" />
+      <div className={styles.flexdiv}>
+        <div>
+          {props.type === "store" ? (
+            <span className={styles.userName}>{props.storeName || props.name}</span>
+          ) : (
+            <span className={styles.userName}>{props.nickname || props.name}</span>
+          )}
+          <Link href="/mypage/settings">
+            <a className={styles.settings}>
+              <span className="material-icons-outlined">settings</span>
+            </a>
+          </Link>
+        </div>
+        {props.type === "store" && (
+          <div className={styles.profile_seller}>
+            <p>판매자 프로필 하단</p>
+            <Contents rating={props.rating} introduce={props.introduce} feedsNum={props.feedsNum} />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default HeaderItem;
