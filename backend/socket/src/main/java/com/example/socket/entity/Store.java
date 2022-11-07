5cd772b2e913a19951cbd488c7a0ab54ecf269a5
박수근 <clapans555@gmail.com>
2022-11-07 11:23:35 +0900

    Fix: 채팅 > CORS 문제 수정 preflight 설정

---
+++ backend/socket/src/main/java/com/example/socket/entity/Store.java
@@ -0,0 +1,109 @@
+package com.example.socket.entity;
+
+import com.example.socket.domain.UserType;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "store")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Store implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Auto-Increment
+    private Long id;
+
+    @Column(name = "type", nullable = false)
+    @Enumerated(EnumType.STRING)
+    UserType type;
+
+    @Column(name = "user_id", length = 16, nullable = false)
+    private String userId;
+
+    @Column(name = "password", length = 100, nullable = false)
+    private String password;
+
+    @Column(name = "name", length = 30, nullable = false)
+    private String name;
+
+    @Column(name = "store", length = 50, nullable = false)
+    private String store;
+
+    @Column(name = "license", length = 12, nullable = false)
+    private String license;
+
+    @Column(name = "email", length = 50, nullable = false)
+    private String email;
+
+    @Column(name = "profile", nullable = true)
+    @Lob
+    private String profile;
+
+    @Column(name = "address", length = 200, nullable = false)
+    private String address;
+
+    // 휴일은 나중에 작업
+//    @Column(name = "holidays", nullable = true)
+//    private String holidays;
+//
+    @Column(name = "bio", length = 300, nullable = true)
+    private String bio;
+
+    @Column(name = "reg_date", nullable = true)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date regDate;
+
+    @Column(name = "withdrawal")
+    private boolean withdrawal;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+
+        authorities.add(new SimpleGrantedAuthority("USER"));
+
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return userId;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
