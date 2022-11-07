c4e7f3ac418309a3734d984efe21db1a9988ec99
dabeen <elqla19@gmail.com>
2022-11-07 15:59:25 +0900

    Feat: 구매자 마이페이지 예약목록 디테일

--- frontend/src/pages/mypage/reservation/[rid].js
+++ frontend/src/pages/mypage/reservation/[rid].js
@@ -1,8 +1,6 @@
 import MyHeader from "@/components/mypage/MyHeader";
 import MyWrapper from "@/components/common/MyWrapper";
 import ResDetail from "@/components/mypage/ResDetail";
-import React from "react";
-import styles from "./resdetail.module.scss";
 
 const ReservationDetail = () => {
   return (
