e1035b1c8b9363b6db1dcff559327729653c3821
성유지 <anc9432@gmail.com>
2022-11-08 22:15:17 +0900

    Feat: 커스텀 > Redux-persist 적용 중

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,9 +2,21 @@ import { configureStore } from "@reduxjs/toolkit";
 import { createWrapper } from "next-redux-wrapper";
 import rootReducer from "./reducers";
 
+import { persistReducer } from "redux-persist";
+import storageSession from "redux-persist/lib/storage/session";
+
+// * session storage
+const persistConfig = {
+  key: "root",
+  storage: storageSession,
+  whitelist: ["custom"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const createStore = () => {
   const store = configureStore({
-    reducer: rootReducer,
+    reducer: persistedReducer,
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(),
     devTools: true,
   });
