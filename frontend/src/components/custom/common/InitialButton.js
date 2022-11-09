b15dfe36be778745cc8436d89c2a77777a429884
Youji-Sung <anc9432@gmail.com>
2022-11-09 17:52:38 +0900

    Feat: 커스텀 > 초기화 버튼 작업 중

---
+++ frontend/src/components/custom/common/InitialButton.js
@@ -0,0 +1,20 @@
+import styles from "./InitialButton.module.scss";
+import { useDispatch, useSelector } from "react-redux";
+import { makeFlowerList } from "@/store/reducers/custom";
+
+const InitialButton = () => {
+  const dispatch = useDispatch();
+  const flowerList = useSelector((state) => state.custom.flowers);
+  const flowerInitialHandler = () => {
+    const newFlowerList = flowerList.fill(0);
+    dispatch(makeFlowerList(newFlowerList));
+  };
+
+  return (
+    <button className={styles.initial_button} onClick={() => flowerInitialHandler()}>
+      초기화
+    </button>
+  );
+};
+
+export default InitialButton;
