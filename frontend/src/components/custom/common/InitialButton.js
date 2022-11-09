168ab0c5273b58d951dc148e902927f16231cf48
성유지 <anc9432@gmail.com>
2022-11-09 23:53:01 +0900

    Fix: 커스텀 > 초기화 버튼으로 리본, 포장지까지 초기화되도록 수정

--- frontend/src/components/custom/common/InitialButton.js
+++ frontend/src/components/custom/common/InitialButton.js
@@ -1,14 +1,21 @@
 import styles from "./InitialButton.module.scss";
 import { useDispatch, useSelector } from "react-redux";
-import { makeFlowerList } from "@/store/reducers/custom";
+import {
+  makeFlowerList,
+  selectWrapperColor,
+  selectRibbonColor,
+} from "@/store/reducers/custom";
 
 const InitialButton = () => {
   const dispatch = useDispatch();
   const flowerList = useSelector((state) => state.custom.flowers);
+
   const flowerInitialHandler = () => {
     const copyOfFlowerList = [...flowerList];
     const newFlowerList = copyOfFlowerList.fill(0);
     dispatch(makeFlowerList(newFlowerList));
+    dispatch(selectWrapperColor(null));
+    dispatch(selectRibbonColor(null));
   };
 
   return (
