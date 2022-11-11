ee7cab00893b554970fde1f19e0f75a43a879855
dabeen <elqla19@gmail.com>
2022-11-11 14:08:17 +0900

    Feat: 구매자 마이페이지 > 예약 목록 및 디테일 로직 연결

--- frontend/src/pages/mypage/reservation/[rid].js
+++ frontend/src/pages/mypage/reservation/[rid].js
@@ -1,6 +1,7 @@
 import MyHeader from "@/components/mypage/MyHeader";
 import MyWrapper from "@/components/common/MyWrapper";
 import ResDetail from "@/components/mypage/ResDetail";
+import useSWR from "swr";
 
 const ReservationDetail = () => {
   return (
