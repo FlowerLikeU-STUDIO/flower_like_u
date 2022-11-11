9e2d1cbeab04e311cce713c9078beb6270596156
성유지 <anc9432@gmail.com>
2022-11-11 21:32:41 +0900

    Fix: 커스텀 > 비로그인 상태일 때 alert 발생 함수 추가, 포장지와 리본을 골랐을 경우에 라우팅

--- frontend/src/components/custom/step/BouquetCustom.js
+++ frontend/src/components/custom/step/BouquetCustom.js
@@ -14,11 +14,14 @@ import { mutate } from "swr";
 import { client } from "@/pages/api/client";
 import useSWR from "swr";
 import storage from "@/lib/utils/storage";
+import { useRouter } from "next/router";
 
 const BuoquetCustom = () => {
   const cx = classNames.bind(styles);
+  const router = useRouter();
   const { data: isLogin } = useSWR("logIn", storage);
   const customOption = useSelector((state) => state.custom);
+  console.log(customOption);
   //* 유저가 선택한 패키지 종류
   const packageKind = packageContent.engtitle[customOption.package];
 
@@ -43,17 +46,6 @@ const BuoquetCustom = () => {
     });
   };
   const saveCustom = async (myImg) => {
-    //* 사이즈 정보
-    let sizeList = [];
-    customOption.package === 0 ? (sizeList = ["XS", "S", "M", "L", "XL"]) : (sizeList = ["S", "M", "L"]);
-
-    //* 꽃 정보
-    const flowers = customOption.flowers;
-    let flowers_data = [];
-    for (let i = 0; i < flowers.length; i++) {
-      flowers_data.push(flowers[i] + 1);
-    }
-
     //* 옵션을 고르지 않았을 때 alert 발생
     if (customOption.package === 0 && customOption.wrapper_color === null) {
       FailAlert("포장지 색을 골라주세요!");
@@ -62,9 +54,25 @@ const BuoquetCustom = () => {
     if (customOption.package === 2 && customOption.wrapper_color === null) {
       FailAlert("포장지 색을 골라주세요!");
       return;
-    } else if (customOption.package === 0 && customOption.ribbon_color === null) {
+    } else if (
+      customOption.package === 0 &&
+      customOption.ribbon_color === null
+    ) {
       FailAlert("리본 색을 골라주세요!");
       return;
+    }
+
+    //* 사이즈 정보
+    let sizeList = [];
+    customOption.package === 0
+      ? (sizeList = ["XS", "S", "M", "L", "XL"])
+      : (sizeList = ["S", "M", "L"]);
+
+    //* 꽃 정보
+    const flowers = customOption.flowers;
+    let flowers_data = [];
+    for (let i = 0; i < flowers.length; i++) {
+      flowers_data.push(flowers[i] + 1);
     }
 
     //* 포장지, 리본 정보
@@ -93,6 +101,26 @@ const BuoquetCustom = () => {
     } else {
       alert("실패");
     }
+    router.push("/custom/save");
+  };
+
+  const notUserSaveCustom = () => {
+    //* 옵션을 고르지 않았을 때 alert 발생
+    if (customOption.package === 0 && customOption.wrapper_color === null) {
+      FailAlert("포장지 색을 골라주세요!");
+      return;
+    }
+    if (customOption.package === 2 && customOption.wrapper_color === null) {
+      FailAlert("포장지 색을 골라주세요!");
+      return;
+    } else if (
+      customOption.package === 0 &&
+      customOption.ribbon_color === null
+    ) {
+      FailAlert("리본 색을 골라주세요!");
+      return;
+    }
+    router.push("/custom/save");
   };
 
   return (
@@ -120,12 +148,19 @@ const BuoquetCustom = () => {
           </div>
         </div>
         <div className={styles.custom_info_wrapper}>
-          <p className={styles.custom_info_package}>{SizeContent[customOption.package].kotitle} 커스텀</p>
-          <p className={styles.custom_info_size}>{SizeContent[customOption.package].title[customOption.size]} 사이즈</p>
-          <button className={styles.go_save_page}>
-            <Link href="/custom/save">
-              <div onClick={isLogin ? () => onSaveCustomInfo() : () => console.log("완성")}>완성!</div>
-            </Link>
+          <p className={styles.custom_info_package}>
+            {SizeContent[customOption.package].kotitle} 커스텀
+          </p>
+          <p className={styles.custom_info_size}>
+            {SizeContent[customOption.package].title[customOption.size]} 사이즈
+          </p>
+          <button
+            className={styles.go_save_page}
+            onClick={
+              isLogin ? () => onSaveCustomInfo() : () => notUserSaveCustom()
+            }
+          >
+            완성!
           </button>
         </div>
         <CustomMenu />
