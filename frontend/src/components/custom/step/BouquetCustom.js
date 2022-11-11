4afc2eea904949392d0ef1ac456596d213e05f34
Youji-Sung <anc9432@gmail.com>
2022-11-11 13:18:36 +0900

    Feat: 커스텀 > 커스텀 옵션 저장 데이터 생성

--- frontend/src/components/custom/step/BouquetCustom.js
+++ frontend/src/components/custom/step/BouquetCustom.js
@@ -10,6 +10,8 @@ import InitialButton from "../common/InitialButton";
 import html2canvas from "html2canvas";
 import { saveAs } from "file-saver";
 import RandomFlower from "./recommend/RandomFlower";
+import { wrapper } from "./menu/MenuContents";
+import FailAlert from "@/lib/FailAlert";
 
 const BuoquetCustom = () => {
   const cx = classNames.bind(styles);
@@ -17,8 +19,15 @@ const BuoquetCustom = () => {
   const customOption = useSelector((state) => state.custom);
   //* 유저가 선택한 패키지 종류
   const packageKind = packageContent.engtitle[customOption.package];
-  //* 유저가 선택한 포장지 색 종류
-  const packageColor = customOption.wrapper_color;
+
+  //* 유저가 포장지를 선택할 때 색상을 변경해 주는 로직
+  let packageColor = "#dfd4b9";
+  customOption.wrapper_color === null
+    ? (packageColor = "#ffffff")
+    : customOption.wrapper_color
+    ? (packageColor = wrapper[customOption.wrapper_color].hex)
+    : (packageColor = wrapper[customOption.wrapper_color].hex);
+
   //* 유저가 선택한 리본 종류
   const ribbonOption = customOption.ribbon_color;
 
@@ -34,12 +43,77 @@ const BuoquetCustom = () => {
     });
   };
 
+  const makeBase64Button = () => {
+    const capture = document.querySelector("#capture");
+    html2canvas(capture).then((canvas) => {
+      let myImg = canvas.toDataURL("image/png");
+      myImg = myImg.replace("data:image/png;base64,", "");
+    });
+  };
+
+  //* 완성해서 서버에 이미지랑 데이터 보내기
+  const saveCustom = async (res) => {
+    //* 이미지 base64 인코딩
+    const capture = document.querySelector("#capture");
+    html2canvas(capture).then((canvas) => {
+      let myImg = canvas.toDataURL("image/png");
+      myImg = myImg.replace("data:image/png;base64,", "");
+
+      //* 사이즈 정보
+      let sizeList = [];
+      customOption.package === 0 ? (sizeList = ["XS", "S", "M", "L", "XL"]) : (sizeList = ["S", "M", "L"]);
+
+      //* 꽃 정보
+      const flowers = customOption.flowers;
+      let flowers_data = [];
+      for (let i = 0; i < flowers.length; i++) {
+        flowers_data.push(flowers[i] + 1);
+      }
+
+      //* 옵션을 고르지 않았을 때 alert 발생
+      if (customOption.package === 0 && customOption.wrapper_color === null) {
+        FailAlert("포장지 색을 골라주세요!");
+        return;
+      }
+      if (customOption.package === 2 && customOption.wrapper_color === null) {
+        FailAlert("포장지 색을 골라주세요!");
+        return;
+      } else if (customOption.package === 0 && customOption.ribbon_color === null) {
+        FailAlert("리본 색을 골라주세요!");
+        return;
+      }
+
+      //* 포장지, 리본 정보
+      let wrapperId = null;
+      let ribbonId = null;
+      if (customOption.package === 0) {
+        wrapperId = customOption.wrapper_color + 1;
+        ribbonId = customOption.ribbon_color + 1;
+      } else if (customOption.package === 2) {
+        wrapperId = customOption.wrapper_color + 1;
+      }
+
+      const data = {
+        type: packageContent.engtitle[customOption.package],
+        image: myImg,
+        wrapperId: wrapperId,
+        ribbonId: ribbonId,
+        size: sizeList[customOption.size],
+        flowers: flowers_data,
+      };
+      console.log(data);
+    });
+  };
+
   return (
     <>
       <main className={styles.custom_wrapper}>
         <aside className={styles.recommend_wrapper}>
           <RandomFlower />
-          <div className={styles.recommend_menu}>추천</div>
+          <button className={styles.recommend_menu} onClick={() => saveCustom()}>
+            saveCustom
+          </button>
+
           <div className={styles.recommend_menu}>추천</div>
           <InitialButton />
           <button className={styles.save_button} onClick={() => onDownloadButton()}>
