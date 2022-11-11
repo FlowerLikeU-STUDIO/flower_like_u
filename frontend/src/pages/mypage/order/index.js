83efa66a69d711d5f888f6ac6724bebe70d1ea79
dabeen <elqla19@gmail.com>
2022-11-11 14:08:51 +0900

    Feat: 구매자 마이페이지 > 주문내역 로직 연결

--- frontend/src/pages/mypage/order/index.js
+++ frontend/src/pages/mypage/order/index.js
@@ -2,136 +2,57 @@ import MyHeader from "@/components/mypage/MyHeader";
 import MyWrapper from "@/components/common/MyWrapper";
 import MyListItem from "@/components/mypage/MyListItem";
 import styles from "./index.module.scss";
+import useRes from "@/hooks/useRes";
+import PageNation from "@/components/common/PageNation";
+import { useEffect, useState } from "react";
 
 const CustomerOrderList = () => {
-  const list = [
-    {
-      bookId: 1,
-      type: "custom",
-      storeName: "flowerhouse",
-      image: "",
-      request:
-        "가나다라마바사 가나다라마바사 가나다라마바사 가나다라마바사 가나다라마바사 가나다라마바사 가나다라마바사 가나다라마바사 가나다라마바사 가나다라마바사 가나다라마바사 가나다라마바사 가나다라 (100자)",
-      bookDate: "2022-10-25",
-      dueDate: "2022-10-28",
-      state: "inProgress",
-    },
-    {
-      bookId: 2,
-      type: "feed",
-      storeName: "flowerhouse1",
-      image: "",
-      request: "예쁘게 부탁해요.",
-      bookDate: "2022-10-25",
-      dueDate: "2022-10-28",
-      state: "waited",
-    },
-    {
-      bookId: 3,
-      type: "feed",
-      storeName: "flowerhouse1",
-      image: "",
-      request: "예쁘게 부탁해요.",
-      bookDate: "2022-10-25",
-      dueDate: "2022-10-28",
-      state: "waited",
-    },
-    {
-      bookId: 4,
-      type: "feed",
-      storeName: "flowerhouse1",
-      image: "",
-      request: "예쁘게 부탁해요.",
-      bookDate: "2022-10-25",
-      dueDate: "2022-10-28",
-      state: "waited",
-    },
-    {
-      bookId: 5,
-      type: "feed",
-      storeName: "flowerhouse1",
-      image: "",
-      request: "예쁘게 부탁해요.",
-      bookDate: "2022-10-25",
-      dueDate: "2022-10-28",
-      state: "waited",
-    },
-    {
-      bookId: 6,
-      type: "feed",
-      storeName: "flowerhouse1",
-      image: "",
-      request: "예쁘게 부탁해요.",
-      bookDate: "2022-10-25",
-      dueDate: "2022-10-28",
-      state: "waited",
-    },
-    {
-      bookId: 7,
-      type: "feed",
-      storeName: "flowerhouse1",
-      image: "",
-      request: "예쁘게 부탁해요.",
-      bookDate: "2022-10-25",
-      dueDate: "2022-10-28",
-      state: "waited",
-    },
-    {
-      bookId: 8,
-      type: "feed",
-      storeName: "flowerhouse1",
-      image: "",
-      request: "예쁘게 부탁해요.",
-      bookDate: "2022-10-25",
-      dueDate: "2022-10-28",
-      state: "waited",
-    },
-    {
-      bookId: 9,
-      type: "feed",
-      storeName: "flowerhouse1",
-      image: "",
-      request: "예쁘게 부탁해요.",
-      bookDate: "2022-10-25",
-      dueDate: "2022-10-28",
-      state: "waited",
-    },
-    {
-      bookId: 10,
-      type: "feed",
-      storeName: "flowerhouse1",
-      image: "",
-      request: "예쁘게 부탁해요.",
-      bookDate: "2022-10-25",
-      dueDate: "2022-10-28",
-      state: "waited",
-    },
-    {
-      bookId: 11,
-      type: "feed",
-      storeName: "flowerhouse1",
-      image: "",
-      request: "예쁘게 부탁해요.",
-      bookDate: "2022-10-25",
-      dueDate: "2022-10-28",
-      state: "waited",
-    },
-  ];
+  const { orderList } = useRes();
+  const [pageIndex, setPageIndex] = useState(1);
+  const { data, maxPage, mutate } = orderList({ pageIndex });
+  const [numLst, setNumList] = useState([1]); //[ 1, 2, 3, 4, 5]
+  const [selectNumLst, setSelectNumList] = useState([1]); //[ 1, 2, 3], [ 4, 5 ]
+
+  useEffect(() => {
+    if (!data) return;
+    const setPage = () => {
+      if (maxPage === 1) return;
+      let tmpNumLst = [];
+      for (let i = 1; i <= maxPage; i++) {
+        tmpNumLst.push(i);
+      }
+      setNumList(tmpNumLst);
+    };
+    setPage();
+  }, [data]);
+
+  const setCurrentPage = () => {
+    setSelectNumList(numLst.slice(Math.floor((pageIndex - 1) / 3) * 3, Math.floor((pageIndex - 1) / 3) * 3 + 3));
+    mutate();
+  };
+
+  useEffect(() => {
+    if (!data) return;
+    setCurrentPage();
+  }, [pageIndex, numLst]);
+
   return (
     <MyWrapper>
       <MyHeader />
       <div className={styles.main__div}>
-        {list.map((res, idx) => (
-          <div key={idx}>
-            <MyListItem
-              bookId={res.bookId}
-              image={res.image}
-              storeName={res.storeName}
-              request={res.request}
-              dueDate={res.dueDate}
-            />
-          </div>
-        ))}
+        {data &&
+          data.map((res, idx) => (
+            <div key={idx}>
+              <MyListItem
+                bookId={res.bookId}
+                image={res.image}
+                storeName={res.storeName}
+                request={res.request}
+                dueDate={res.dueDate}
+              />
+            </div>
+          ))}
+        <PageNation setPageIndex={setPageIndex} selectNumLst={selectNumLst} pageIndex={pageIndex} maxPage={maxPage} />
       </div>
     </MyWrapper>
   );
