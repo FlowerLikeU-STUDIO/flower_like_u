546d4a610552bf32205c737214c86c7ed9eb6b19
성유지 <anc9432@gmail.com>
2022-11-13 22:00:06 +0900

    Feat: 커스텀 > 상황별 추천 컨텐츠 파일 제작

--- frontend/src/components/custom/step/recommend/situation/SituationButton.js
+++ frontend/src/components/custom/step/recommend/situation/SituationButton.js
@@ -1,8 +1,8 @@
-import styles from "./FlowerSituation.module.scss";
-import SituationModal from "./SituationModal";
+import styles from "./SituationButton.module.scss";
 import { useState } from "react";
+import CustomModal from "@/components/custom/common/CustomModal";
 
-const FlowerSituation = () => {
+const SituationButton = () => {
   // 모달창 노출 여부 state
   const [modalOpen, setModalOpen] = useState(false);
 
@@ -16,9 +16,9 @@ const FlowerSituation = () => {
       <button className={styles.recommend_menu} onClick={showModal}>
         레시피
       </button>
-      {modalOpen && <SituationModal setModalOpen={setModalOpen} />}
+      {modalOpen && <CustomModal setModalOpen={setModalOpen} id={2} />}
     </div>
   );
 };
 
-export default FlowerSituation;
+export default SituationButton;
