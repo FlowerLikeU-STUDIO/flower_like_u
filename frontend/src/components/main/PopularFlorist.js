dc3aff59016044b95950919fbbf031c28f625bd5
성유지 <anc9432@gmail.com>
2022-11-15 22:31:06 +0900

    Feat: 메인 > 메인 페이지 인기있는 꽃집 로직 작성

--- frontend/src/components/main/PopularFlorist.js
+++ frontend/src/components/main/PopularFlorist.js
@@ -1,56 +1,61 @@
 import styles from "./PopularFlorist.module.scss";
 import FlowerImg from "../common/FlowerImg";
 import { client } from "@/pages/api/client";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { Rating } from "@mui/material";
 
 const PopularFlorist = () => {
-  //* 필요한 params 넣어서 api 요청 필요
+  const router = useRouter();
+
+  //* 인기있는 꽃집 요청 (별점 순)
+  const [popular, setPopular] = useState();
+
   const getPopular = async () => {
-    const res = await client.get(`user/stores`).then((res) => res.data);
+    const res = await client
+      .get(`user/stores?page=1&size=4&sort=rating`)
+      .then((res) => res.data);
     if (res) {
-      console.log(res);
+      setPopular(res.storeInfo.list);
     }
   };
+
+  useEffect(() => getPopular, []);
+
   return (
     <section className={styles.popular_wrapper}>
       <h1 className={styles.popular_title}>인기 플로리스트</h1>
-      <div className={styles.florist_wrapper}>
-        <article className={styles.florist_card}>
-          <div className={styles.card_img}>
-            <FlowerImg src="/auth/floristDefaultProfile.png" alt={"너닯꽃"} />
-          </div>
-          <h2 className={styles.florist_name}>꽃풍선 장인</h2>
-          <span className={styles.florist_address}>
-            대전광역시 구암동 678-2 어울림
-          </span>
-        </article>
-        <article className={styles.florist_card}>
-          <div className={styles.card_img}>
-            <FlowerImg src="/home/3.png" alt={"너닯꽃"} />
-          </div>
-          <h2 className={styles.florist_name}>싸피꽃집</h2>
-          <span className={styles.florist_address}>
-            대전광역시 유성구 동서대로 98-39
-          </span>
-        </article>
-        <article className={styles.florist_card}>
-          <div className={styles.card_img}>
-            <FlowerImg src="/home/9.png" alt={"너닯꽃"} />
-          </div>
-          <h2 className={styles.florist_name}>고래꽃집</h2>
-          <span className={styles.florist_address}>
-            울산광역시 남구 옥동 은월로 2번길
-          </span>
-        </article>
-        <article className={styles.florist_card}>
-          <div className={styles.card_img}>
-            <FlowerImg src="/home/5.png" alt={"너닯꽃"} />
-          </div>
-          <h2 className={styles.florist_name}>강릉꽃집</h2>
-          <span className={styles.florist_address}>
-            강원 강릉시 강릉대로 33
-          </span>
-        </article>
-      </div>
+      <section className={styles.florist_card_wrapper}>
+        {popular ? (
+          popular.map((florist) => (
+            <article
+              key={florist.storeId}
+              className={styles.florist__wrapper}
+              onClick={() => router.push(`/florist/${florist.storeId}`)}
+            >
+              <div className={styles.store__img}>
+                <FlowerImg src={florist.profile} florist={"florist"} />
+              </div>
+              <div className={styles.store__info}>
+                <p className={styles.store__name}>{florist.storeName}</p>
+                <p className={styles.store__adderss}>{florist.address}</p>
+                <div className={styles.store__star}>
+                  <Rating
+                    defaultValue={florist.rating}
+                    size="medium"
+                    precision={0.5}
+                    readOnly
+                    className={styles.starrating}
+                  />
+                  <span>{florist.rating}점</span>
+                </div>
+              </div>
+            </article>
+          ))
+        ) : (
+          <></>
+        )}
+      </section>
     </section>
   );
 };
