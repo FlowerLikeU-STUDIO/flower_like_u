8056a874c201378878ef313a47f1836ba51f8130
강민성 <pfcskms1997@naver.com>
2022-11-16 14:47:54 +0900

    Modify: 리뷰 > 목록 API 응답에 프로필이미지, 날짜 추가

--- backend/fly/src/main/java/com/ssafy/fly/dto/response/ReviewInfoRes.java
+++ backend/fly/src/main/java/com/ssafy/fly/dto/response/ReviewInfoRes.java
@@ -3,11 +3,15 @@ package com.ssafy.fly.dto.response;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 @Builder
 public class ReviewInfoRes {
     private Long reviewId;
     private String writer;
+    private String writerProfile;
     private Double rating;
     private String content;
+    private String regDate;
 }
