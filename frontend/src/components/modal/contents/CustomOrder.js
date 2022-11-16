86e518bf30733cc0ac0571c9a6f6baae7712c930
dabeen <elqla19@gmail.com>
2022-11-16 21:52:03 +0900

    Feat: 내 디자인 주문 > API연결위한 데이터 테스트

--- frontend/src/components/modal/contents/CustomOrder.js
+++ frontend/src/components/modal/contents/CustomOrder.js
@@ -1,11 +1,55 @@
 import CustomFlorist from "@/components/custom/order/CustomFlorist";
 import CustomReservation from "@/components/custom/order/CustomReservation";
-import { useState } from "react";
+import { client } from "@/pages/api/client";
+import { useEffect, useState } from "react";
 
 const CustomOrder = ({ orderStep }) => {
   const [step, setStep] = useState(orderStep);
+  const [storeId, setStoreId] = useState();
+  const [flowerId, setFlowerId] = useState();
+  const [request, setRequest] = useState();
+  const [dueDate, setDueDate] = useState();
 
-  return <>{step === "florist" ? <CustomFlorist setStep={setStep} /> : <CustomReservation setStep={setStep} />}</>;
+  // *debugging start
+  const data = {
+    storeId: storeId,
+    flowerId: flowerId,
+    request: request,
+    dueDate: dueDate, // @예약날짜
+  };
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+  // *debugging end
+
+  // 예약 데이터 제출
+  const submitData = () => {
+    const data = {
+      storeId: storeId,
+      flowerId: flowerId,
+      request: request,
+      dueDate: dueDate,
+    };
+    if (!storeId) {
+      alert("스토어를 선택해주세요");
+      return;
+    }
+    const res = client.post("book/custom", data).then((res) => res.data);
+    if (res.result === "success") {
+      alert("성공적으로 예약되었습니다.");
+      return;
+    }
+  };
+
+  return (
+    <>
+      {step === "florist" ? (
+        <CustomFlorist storeId={storeId} setStep={setStep} setStoreId={setStoreId} />
+      ) : (
+        <CustomReservation setStep={setStep} />
+      )}
+    </>
+  );
 };
 
 export default CustomOrder;
