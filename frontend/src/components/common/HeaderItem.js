227f86493e75ddbbb92d4aa0a9542e2895ae483d
dabeen <elqla19@gmail.com>
2022-11-17 10:10:52 +0900

    Feat: 마이페이지 > 헤더아이템 버튼 추가

--- frontend/src/components/common/HeaderItem.js
+++ frontend/src/components/common/HeaderItem.js
@@ -11,20 +11,35 @@ const HeaderItem = ({ ...props }) => {
       <div className={styles.header__main}>
         <ProfileImage url={props.profile} size="medium" />
         <div className={styles.flexdiv}>
-          <div>
-            {props.type === "store" ? (
-              <span className={styles.userName}>{props.storeName || props.name}</span>
-            ) : (
-              <span className={styles.userName}>{props.nickname || props.name}</span>
-            )}
-            {props.isMyPage && (
-              <Link href="/mypage/settings">
-                <a className={styles.settings}>
-                  <span className="material-icons-outlined">settings</span>
-                </a>
-              </Link>
+          <div className={styles.store__mypage}>
+            <div>
+              {props.type === "store" ? (
+                <span className={styles.userName}>{props.storeName || props.name}</span>
+              ) : (
+                <span className={styles.userName}>{props.nickname || props.name}</span>
+              )}
+              {props.isMyPage && (
+                <>
+                  <Link href="/mypage/settings">
+                    <a className={styles.settings}>
+                      <span className="material-icons-outlined">settings</span>
+                    </a>
+                  </Link>
+                </>
+              )}
+            </div>
+            {props.type === "store" && (
+              <button
+                className={styles.feed}
+                onClick={() => {
+                  console.log("피드등록 로직");
+                }}
+              >
+                피드 등록하기
+              </button>
             )}
           </div>
+
           {props.type === "store" && (
             <div className={styles.profile_seller}>
               <p>판매자 프로필 하단</p>
